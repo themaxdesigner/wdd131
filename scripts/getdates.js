f9225c564f7b1f06bbdfb541c06e364aa20e3494
themaxdesigner <igb24002@byui.edu>
2024-09-21 17:05:12 +0100

    api

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the current year
+    
     const currentYear = new Date().getFullYear();
-    // Set the current year in the footer's first paragraph
     document.getElementById("currentyear").textContent = currentYear;
 
-    // Get the last modified date of the document
     const lastModifiedDate = document.lastModified;
-    // Set the last modified date in the second paragraph
+   
     document.getElementById("lastModified").textContent = `Last modified: ${lastModifiedDate}`;
 });
